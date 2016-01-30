7ad35210d1e13e3c300b3f5470d8780247e9fbdf
Gustavo Ribeiro <gustavogonzagaribeiro@gmail.com>
2016-01-30 11:52:21 -0600

    First commit

---
+++ index.php
@@ -0,0 +1,260 @@
+<html>
+<head>
+    <title>Pizza! - Home</title>
+
+    <!-- Compressed Foundation CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.css">
+
+    <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.7/jquery.fullPage.css" />
+
+    <!-- Styled CSS -->
+    <link rel="stylesheet" href="resources/stylesheet/style.css" />
+
+    <!-- Including jquery -->
+    <script src="https://code.jquery.com/jquery-2.2.0.min.js"></script>
+
+    <!-- Compressed Foundation JavaScript -->
+    <script src="https://cdn.jsdelivr.net/foundation/6.1.2/foundation.min.js"></script>
+
+    <!-- Including the fonts -->
+    <link href='https://fonts.googleapis.com/css?family=PT+Sans+Narrow:400,700' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Pacifico' rel='stylesheet' type='text/css'>
+
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.7.7/jquery.fullPage.js"></script>
+
+</head>
+<body>
+
+<div id="fullpage">
+    <div class="section home">
+        <div class="row">
+            <div class="home-content">
+                <a href="javascript:void(0)"><img src="resources/img/go-1.png" class="home-button" id="b" /></a>
+                <h1>ARE YOU HUNGRY?</h1>
+                <h3>How about trying our delicious pizza?</h3>
+
+            </div>
+        </div>
+    </div>
+
+    <div class="order" id="order">
+        <div class="small-8 small-centered columns order-background">
+            <div>
+                <img src="resources/img/logo-2.png" class="order-title">
+                <spam class="order-title">Create your own pizza!</spam>
+                <br><br>
+                <div class="row">
+                    <div class="order-steps">
+                        <span class="badge order-bullet" id="messageCount">1</span>
+                        <span class="order-subtitle">Pick a size:</span>
+                    </div>
+                    <br>
+                    <div class="small-12 large-4 columns">
+                        <div class="pizza">
+                            <img src="resources/img/pizza-1.png" class="pizza">
+                            <p>Large</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-4 columns">
+                        <div class="pizza">
+                            <img src="resources/img/pizza-2.png" class="pizza" style="padding-top: 20px">
+                            <p>Medium</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-4 columns">
+                        <div class="pizza">
+                            <img src="resources/img/pizza-3.png" class="pizza" style="padding-top: 30px">
+                            <p>Small</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="small-12">
+                        <br>
+                        <center>
+                            <img src="resources/img/dash.png">
+                        </center>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="order-steps">
+                        <span class="badge order-bullet" id="messageCount">2</span>
+                        <span class="order-subtitle">Choose your sauce:</span>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/sauce-1.png" class="pizza">
+                            <p>Marinara</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/sauce-2.png" class="pizza">
+                            <p>Barbecue</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/sauce-3.png" class="pizza">
+                            <p>Parmesan</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/sauce-4.png" class="pizza">
+                            <p>Buffalo</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="small-12">
+                        <br>
+                        <center>
+                            <img src="resources/img/dash.png">
+                        </center>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="order-steps">
+                        <span class="badge order-bullet" id="messageCount">3</span>
+                        <span class="order-subtitle">Do you want cheese?</span>
+                    </div>
+                    <br>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/cheese.png" class="pizza">
+                            <p>Yes, please</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-9 columns">
+                        <div class="pizza" style="text-align: left">
+                            <img src="resources/img/cheese.png" class="pizza" style="opacity: 0">
+                            <p>No, thanks</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="small-12">
+                        <br>
+                        <center>
+                            <img src="resources/img/dash.png">
+                        </center>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="order-steps">
+                        <span class="badge order-bullet" id="messageCount">4</span>
+                        <span class="order-subtitle">Toppings:</span>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/topping-1.png" class="pizza" style="padding-top: 20px">
+                            <p>Peperoni</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/topping-2.png" class="pizza">
+                            <p>Bacon</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/topping-3.png" class="pizza" style="padding-top: 20px">
+                            <p>Ham</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/topping-4.png" class="pizza" style="padding-top: 5px">
+                            <p>Canandian Ham</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="small-12">
+                        <br>
+                        <center>
+                            <img src="resources/img/dash.png">
+                        </center>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="order-steps">
+                        <span class="badge order-bullet" id="messageCount">5</span>
+                        <span class="order-subtitle">Vaggies</span>
+                    </div>
+                    <br>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <img src="resources/img/veggie-1.png" class="pizza">
+                            <p>Bell pepper</p>
+                        </div>
+                    </div>
+                    <div class="small-12 large-9 columns">
+                        <div class="pizza" style="text-align: left">
+                            <img src="resources/img/veggie-2.png" style="padding-top: 13px">
+                            <p>Olives</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="small-12">
+                        <br>
+                        <center>
+                            <img src="resources/img/dash.png">
+                        </center>
+                        <br>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="small-12 large-9 columns">
+                        <div class="pizza">
+                            ready?
+                        </div>
+                    </div>
+                    <div class="small-12 large-3 columns">
+                        <div class="pizza">
+                            <a href="javascript:void(0)"><img src="resources/img/go-2.png" ></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<script>
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            autoScrolling: false,
+            fitToSection: false
+        });
+
+        function beeLeft() {
+            $("#b").animate({top: "-=30"}, 1500, "swing", beeRight);
+        }
+        function beeRight() {
+            $("#b").animate({top: "+=30"}, 1500, "swing", beeLeft);
+        }
+
+        beeRight();
+
+        $('.home-button').click(function(){
+
+            $('html, body').animate({
+                scrollTop: $(".order").offset().top
+            }, 2000);
+
+        });
+    });
+
+
+</script>
+</body>
+</html>
